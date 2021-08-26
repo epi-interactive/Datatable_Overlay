7140744cf824ce4b002275d9ea8628ec544eddee
Crystal <crystal@epi-interactive.com>
2021-08-26 17:09:12 +1200

    cleaning up code and updating modal

--- app.R
+++ app.R
@@ -1,44 +1,40 @@
 library(shiny)
 library(DT)
 library(dplyr)
-library(shinyBS)
-
 
 shinyApp(
   ui = fluidPage(
     div(
-      uiOutput("modalDTDialog"),
-      DT::dataTableOutput("tableUI"))),
-  
+      dataTableOutput("tableUI")
+    )
+  ),
   
   server = function(input, output, session) {
-    dat <- iris
-    dat <- dat %>% mutate(GID = 1:nrow(dat))
-    
-    dat <- dat %>% mutate(More = paste0('<span style="float:right;"><a href="javascript:void(0)" onmousedown="',
-                                               'Shiny.onInputChange(\'DTClick\',[', GID, ',Math.random()]);',
-                                               ' event.preventDefault(); event.stopPropagation(); return false;"><font color="grey">&#9679;&#9679;&#9679;&nbsp;&nbsp;</font></a></span>'))
-    
-    output$modalDTDialog <- renderUI({
-      bsModal("DTDialog", "Test Title", "", size="small",
-              div(HTML(paste0("You just clicked row ", as.numeric(input$DTClick[1]))),
-                  uiOutput("modalContent")))
-    })
-    
-    output$tableUI = DT::renderDataTable(
+    output$tableUI = renderDataTable({
+      dat <- iris %>% mutate(More = paste0('<span><a href="javascript:void(0)" onmousedown="',
+                                          'Shiny.onInputChange(\'DTClick\',[', 1:n(), ',Math.random()]);',
+                                          'event.preventDefault(); event.stopPropagation(); return false;"><font color="grey">&#9679;&#9679;&#9679;</font></a></span>'))
+      
       datatable(dat,
                 escape = FALSE)
-    )
+    })
     
-    output$modalContent <- renderUI({
-      div("Just a test")
+    output$modalContent <- renderDataTable({
+      dat <- iris[as.numeric(input$DTClick[1]),]
+      datatable(dat,
+                rownames = FALSE,
+                options = list("lengthChange" = FALSE,
+                               "searching" = FALSE,
+                               "paging" = FALSE,
+                               "info" = FALSE,
+                               "ordering" = FALSE))
     })
       
     observeEvent(input$DTClick, {
       showModal(modalDialog(
-        title = "Sample message",
-        div(HTML(paste0("You just clicked row ", as.numeric(input$DTClick[1]))),
-            uiOutput("modalContent")),
+        title = paste0("You just clicked row ", as.numeric(input$DTClick[1])),
+        dataTableOutput("modalContent"),
+        size = "l",
         easyClose = TRUE,
         footer = NULL
       ))
