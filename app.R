8eee69bcd215378736bdfec2f1ff0247d6a92155
Crystal <crystal@epi-interactive.com>
2021-09-16 16:57:57 +1200

    Styled page

--- app.R
+++ app.R
@@ -10,18 +10,28 @@ library(dplyr)
 
 shinyApp(
   ui = fluidPage(
-    div(
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "css/style.css")
+    ),
+    fluidRow(class="header-row",
+      h1("Datatable overlay"),
+      tags$img(src="images/Epi_Logo.png", width= "200px", height = "30px")
+    ),
+    div(class="main",
       dataTableOutput("tableUI")
     )
   ),
   
   server = function(input, output, session) {
+    names(iris) <- c("Speal length", "Sepal width", "Petal length", "Petal width", "Species")
     output$tableUI = renderDataTable({
       dat <- iris %>% mutate(More = paste0('<span><a href="javascript:void(0)" onmousedown="',
                                           'Shiny.onInputChange(\'DTClick\',[', 1:n(), ',Math.random()]);',
-                                          'event.preventDefault(); event.stopPropagation(); return false;"><font color="grey">&#9679;&#9679;&#9679;</font></a></span>'))
+                                          'event.preventDefault(); event.stopPropagation(); return false;"><font color="grey"><b>...</b></font></a></span>'))
       
       datatable(dat,
+                rownames = FALSE,
+                selection ="none",
                 escape = FALSE)
     })
     
@@ -29,6 +39,7 @@ shinyApp(
       dat <- iris[as.numeric(input$DTClick[1]),]
       datatable(dat,
                 rownames = FALSE,
+                selection ="none",
                 options = list("lengthChange" = FALSE,
                                "searching" = FALSE,
                                "paging" = FALSE,
@@ -38,7 +49,7 @@ shinyApp(
       
     observeEvent(input$DTClick, {
       showModal(modalDialog(
-        title = paste0("You just clicked row ", as.numeric(input$DTClick[1])),
+        title = paste0("Row ", as.numeric(input$DTClick[1])),
         dataTableOutput("modalContent"),
         size = "l",
         easyClose = TRUE,
